a0a76383369e7e05e71865d094df27fdd6ac46ec
KallyaniKalesh <kallyani.kalesh@gmail.com>
2025-03-23 15:28:57 -0700

    Project
    DataBento

--- alphaVantage/alphaVantage/wsgi.py
+++ alphaVantage/alphaVantage/wsgi.py
@@ -7,9 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+#import os
 
-from django.core.wsgi import get_wsgi_application
+#from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alphaVantage.settings')
 
